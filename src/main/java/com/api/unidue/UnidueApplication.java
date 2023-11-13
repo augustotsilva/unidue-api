2124d802a0ffbd762dc43cf270ffcc6cf36c613d
Mateus Pereira <mateus.sle.per@hotmail.com>
2023-11-12 21:05:10 -0300

    Spring boot init

---
+++ src/main/java/com/api/unidue/UnidueApplication.java
@@ -0,0 +1,13 @@
+package com.api.unidue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnidueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnidueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/unidue/UnidueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.unidue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
